7fc8c48d4e1d6e3bbf0c905941eb0892a6edcb58
amitkrazzy <31388351+amitkrazzy@users.noreply.github.com>
2017-08-28 00:59:57 +0530

    Create minmax.py

---
+++ minmax.py
@@ -0,0 +1,16 @@
+def minmax(digits):
+    max = digits[0]
+    min = digits[0]
+
+    for d in digits:
+        if d > max:
+            max = d
+        if d < min:
+            min = d
+    result = (min, max)
+
+    return result
+
+digits = input('Enter integers seperated by space: ')
+digits = [int(i) for i in digits.split()] #conveting string into list of integers
+print(minmax(digits))
